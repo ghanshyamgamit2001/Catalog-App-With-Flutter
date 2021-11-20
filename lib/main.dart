5a2c40c78c64ba7f07f5355d8a06a1c7a3d64ecf
Ghanshyam Gamit <ghanshyamgamit2001@gmail.com>
2021-11-20 23:52:09 +0530

    Day 2 Complited

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,27 @@
+import 'package:catalog/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // ignore: unused_local_variable
+    String name = "Ghanshyam";
+    // double pi = 3.14;
+    // bool isMale = true;
+    // num temp = 30.5;
+
+    // var day = "Tuseday";
+    // const pi = 3.14;
+    // final
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to Flutter devlopment in 30 days"),
-          ),
-        ),
-      )
+        home: HomePage(),
     );
   }
 }
