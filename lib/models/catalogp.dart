aecada9e8b78078b8a92935898f8965965bf036f
Ghanshyam Gamit <ghanshyamgamit2001@gmail.com>
2021-11-23 18:12:28 +0530

    day 15 is comlited

--- lib/models/catalogp.dart
+++ lib/models/catalogp.dart
@@ -1,15 +1,7 @@
-class CatalogModel {
-  static final items = [
-    Item(
-        id: 1,
-        name: "iPhone 12 pro",
-        desc: "Apple iphone 12th generation",
-        price: 999,
-        color: "#33505a",
-        image:
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
-  ];
+import 'dart:convert';
 
+class CatalogModel {
+  static List<Item> items = [];
 }
 
 class Item {
@@ -27,7 +19,24 @@ class Item {
       required this.price,
       required this.color,
       required this.image});
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map["id"],
+      name: map["name"],
+      desc: map["desc"],
+      price: map["price"],
+      color: map["color"],
+      image: map["image"],
+    );
+  }
+
+  toMap() => {
+        "id": id,
+        "name": name,
+        "desc": desc,
+        "price": price,
+        "color": color,
+        "image": image,
+      };
 }
-
-// required is not used in video but whithout required throughing error
-
