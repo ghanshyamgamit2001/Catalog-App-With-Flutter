534d6ae51a2ee76e512f55ce19537b30d11ab9bb
Ghanshyam Gamit <ghanshyamgamit2001@gmail.com>
2021-11-23 15:05:31 +0530

    day 13 is comlited

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,34 @@
 import 'package:catalog/models/catalogp.dart';
 import 'package:catalog/widgets/drawer.dart';
 import 'package:catalog/widgets/item_widgets.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 // ignore: use_key_in_widget_constructors
-class HomePage extends StatelessWidget {
-  
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Ghanshyam";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final  catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final  decodeData = jsonDecode(catalogJson);
+    var productData = decodeData["products"];
+    print(productData);
+  }
 
   @override
   Widget build(BuildContext context) {
