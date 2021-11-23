48eccd4ae68edd3bee770dd1b3a194ba0d0a5939
Ghanshyam Gamit <ghanshyamgamit2001@gmail.com>
2021-11-23 12:50:18 +0530

    day 12 is complited

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,27 +1,31 @@
+import 'package:catalog/models/catalogp.dart';
 import 'package:catalog/widgets/drawer.dart';
+import 'package:catalog/widgets/item_widgets.dart';
 import 'package:flutter/material.dart';
-
-//* Day 11 we lerned about context, constrains
 
 // ignore: use_key_in_widget_constructors
 class HomePage extends StatelessWidget {
-  // const HomePage({Key? key}) : super(key: key);
+  
+  final int days = 30;
+  final String name = "Ghanshyam";
 
   @override
   Widget build(BuildContext context) {
-    final int days = 30;
-    final String name = "Ghanshyam";
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Catalog App",
-        ),
+        title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child:
-              Text("Welcome to Flutter $days devlopment in 30 days by $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidgets(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       // ignore: prefer_const_constructors
